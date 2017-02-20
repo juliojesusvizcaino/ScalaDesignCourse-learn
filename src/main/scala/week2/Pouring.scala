fb92659a13df69e8e273ba91940d997609bae150
Julio Jesús Vizcaíno Molina <juliojesusvizcaino@gmail.com>
2017-02-20 06:06:18 +0100

    Add functions that give a solution

--- src/main/scala/week2/Pouring.scala
+++ src/main/scala/week2/Pouring.scala
@@ -10,13 +10,24 @@ class Pouring(capacity: Vector[Int]) {
 
   // Moves
 
-  trait Move
+  trait Move {
+    def change(state: State): State
+  }
 
-  case class Empty(glass: Int) extends Move
+  case class Empty(glass: Int) extends Move {
+    override def change(state: State): State = state updated(glass, 0)
+  }
 
-  case class Fill(glass: Int) extends Move
+  case class Fill(glass: Int) extends Move {
+    override def change(state: State): State = state updated(glass, capacity(glass))
+  }
 
-  case class Pour(from: Int, to: Int) extends Move
+  case class Pour(from: Int, to: Int) extends Move {
+    override def change(state: State): State = {
+      val amount = state(from) min (capacity(to) - state(to))
+      state updated(from, state(from) - amount) updated(to, state(to) + amount)
+    }
+  }
 
   val glasses = 0 until capacity.length
 
@@ -24,5 +35,36 @@ class Pouring(capacity: Vector[Int]) {
     (for (g <- glasses) yield Empty(g)) ++
       (for (g <- glasses) yield Fill(g)) ++
       (for (from <- glasses; to <- glasses if from != to) yield Pour(from, to))
+
+  // Paths
+
+  class Path(history: List[Move]) {
+    def endState: State = (history foldRight initialState) (_ change _)
+
+    def extend(move: Move) = new Path(move :: history)
+
+    override def toString: String = (history.reverse mkString " ") + "--> " + endState
+  }
+
+  val initialPath = new Path(Nil)
+
+  def from(paths: Set[Path]): Stream[Set[Path]] =
+    if (paths.isEmpty) Stream.empty
+    else {
+      val more = for {
+        path <- paths
+        next <- moves map path.extend
+      } yield next
+      paths #:: from(more)
+    }
+
+  val pathSets = from(Set(initialPath))
+
+  def solutions(target: Int): Stream[Path] =
+    for {
+      pathSet <- pathSets
+      path <- pathSet
+      if path.endState contains target
+    } yield path
 
 }
--- src/main/scala/week2/test.sc
+++ src/main/scala/week2/test.sc
@@ -1,4 +1,8 @@
 import week2.Pouring
 
-val problem = new Pouring(Vector(4, 7))
+val problem = new Pouring(Vector(4, 9))
 problem.moves
+
+problem.pathSets.take(3).toList
+
+problem.solutions(6)
